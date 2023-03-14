225e649d7629cfb78cb81558964a90dc9d94ad7c
Sandrieli13 <tskitishvilisandro@gmail.com>
2023-03-14 12:16:45 -0400

    Update users.spec.js

--- server/api/users.spec.js
+++ server/api/users.spec.js
@@ -18,7 +18,7 @@ describe('User routes', () => {
         .get('/api/users')
         .expect(200)
 
-      expect(res.body).to.be.an('arraeeeey');
+      expect(res.body).to.be.an('array');
       expect(res.body.length).to.equal(2);
     })
   }) // end describe('/api/users')
